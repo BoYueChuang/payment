784f023b95a934767711bc102e7c441de3e3b5c5
BoYueChuang <otis29516368@gmail.com>
2025-03-09 17:36:12 +0800

    修改 vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 指定输出路径
+    emptyOutDir: true, // 清空输出目录
+    assetsDir: ".",   // 讓 Vite 把 JS 和 CSS 放到 `dist/` 根目錄
+  },
   server: {
     host: true, // 讓 Vite 綁定 0.0.0.0，允許內網訪問
     port: 5173, // 可選，設定開發伺服器端口
