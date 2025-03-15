e2bb7a043d5c1e9eae0cf55b96321a337ef37451
BoYueChuang <otis29516368@gmail.com>
2025-03-15 21:36:08 +0800

    拔除 env console

--- src/pages/Confgive.tsx
+++ src/pages/Confgive.tsx
@@ -75,11 +75,6 @@ const CONFGive = () => {
 
     // **初始化設定 **
     useEffect(() => {
-        console.log(import.meta.env.VITE_TAPPAY_APP_ID);
-        console.log(import.meta.env.VITE_TAPPAY_APP_KEY);
-        console.log(import.meta.env.VITE_APPLE_MERCHANT_ID);
-        console.log(import.meta.env.VITE_GOOGLE_MERCHANT_ID);
-
         console.log("TapPay SDK 加載完成");
         TPDirect.setupSDK(
             Number(import.meta.env.VITE_TAPPAY_APP_ID),
