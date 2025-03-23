bf9c87e7169544e83948b77a071f93cfa0166b73
BoYueChuang <otis29516368@gmail.com>
2025-03-23 10:44:06 +0800

    測試環境 ＋ 修改路由

--- src/pages/Confgive.tsx
+++ src/pages/Confgive.tsx
@@ -68,7 +68,7 @@ const CONFGive = () => {
         TPDirect.setupSDK(
             Number(import.meta.env.VITE_TAPPAY_APP_ID),
             import.meta.env.VITE_TAPPAY_APP_KEY || '',
-            'production'
+            'sandbox'
         );
         TPDirect.paymentRequestApi.checkAvailability();
         TPDirect.paymentRequestApi.setupApplePay({
